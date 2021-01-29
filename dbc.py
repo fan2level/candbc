d00413434ea07107e7f32f0b778a480da2889142
fan2level <asinwolf@gmail.com>
2021-01-29 11:34:31 +0900

    Update dbc.py

--- dbc.py
+++ dbc.py
@@ -10,29 +10,6 @@ import re
 
 class fileDBC(object):
     """
-    DBC_file =
-    version
-    new_symbols
-    bit_timing (*obsolete but required*)
-    nodes
-    value_tables
-    messages
-    message_transmitters
-    environment_variables
-    environment_variables_data
-    signal_types
-    comments
-    attribute_definitions
-    sigtype_attr_list
-    attribute_defaults
-    attribute_values
-    value_descriptions
-    category_definitions (*obsolete*)
-    categories (*obsolete*)
-    filter (*obsolete*)
-    signal_type_refs
-    signal_groups
-    signal_extended_value_type_list ;
     """
     def __init__(self, filedbc, encoding='utf-8'):
         self.__file = filedbc
@@ -53,13 +30,6 @@ class fileDBC(object):
                                   'version':m.group('version')}
 
             self.__new_symbols = list()
-            # new_symbols = [ '_NS' ':' ['CM_'] ['BA_DEF_'] ['BA_'] ['VAL_']
-            #                 ['CAT_DEF_'] ['CAT_'] ['FILTER'] ['BA_DEF_DEF_'] ['EV_DATA_']
-            #                 ['ENVVAR_DATA_'] ['SGTYPE_'] ['SGTYPE_VAL_'] ['BA_DEF_SGTYPE_']
-            #                 ['BA_SGTYPE_'] ['SIG_TYPE_REF_'] ['VAL_TABLE_'] ['SIG_GROUP_']
-            #                 ['SIG_VALTYPE_'] ['SIGTYPE_VALTYPE_'] ['BO_TX_BU_']
-            #                 ['BA_DEF_REL_'] ['BA_REL_'] ['BA_DEF_DEF_REL_'] ['BU_SG_REL_']
-            #                 ['BU_EV_REL_'] ['BU_BO_REL_'] ];
             pt_new_symbols = re.compile('(?P<signature>NS_)\s+:(?P<new_symbols>.+?)\n\n', re.M|re.S)
             m = pt_new_symbols.search(self.contents)
             if m is not None:
@@ -83,10 +53,6 @@ class fileDBC(object):
                                 'node_names':node_name.split(' ')}
             
             self.__value_tables = list()
-            # value_tables = {value_table} ;
-            # value_table = 'VAL_TABLE_' value_table_name {value_description} ';' ;
-            # value_table_name = C_identifier ;
-            # value_description = double char_string ;
             pt_value_tables = re.compile('(?P<signature>VAL_TABLE_)\s+(?P<value_table_name>\w+)(?P<value_description>.+?)\s*;', re.M)
             pt_value_description =re.compile('(?P<value>[\d.-]+)\s+\"(?P<description>[\w_\s]+)\"')
             for m in pt_value_tables.finditer(self.contents):
@@ -102,44 +68,12 @@ class fileDBC(object):
                 self.__value_tables.append(value_table)
             
             self.__messages = None
-            # messages = {message} ;
-            # message = BO_ message_id message_name ':' message_size transmitter {signal} ;
-            # message_id = unsigned_integer ;
-            # message_name = C_identifier ;
-            # message_size = unsigned_integer ;
-            # transmitter = node_name | 'Vector__XXX' ;
-            # node_name = C_identifier ;
-            # signal = 'SG_' signal_name multiplexer_indicator ':' start_bit '|'
-            # signal_size '@' byte_order value_type '(' factor ',' offset ')'
-            # '[' minimum '|' maximum ']' unit receiver {',' receiver} ;
-            # signal_name = C_identifier ;
-            # multiplexer_indicator = ' ' | 'M' | m multiplexer_switch_value ;
-            # start_bit = unsigned_integer ;
-            # signal_size = unsigned_integer ;
-            # byte_order = '0' | '1' ; (* 0=little endian, 1=big endian *)
-            # value_type = '+' | '-' ; (* +=unsigned, -=signed *)
-            # factor = double ;
-            # offset = double ;
-            # physical_value = raw_value * factor + offset
-            # raw_value = (physical_value – offset) / factor
-            # minimum = double ;
-            # maximum = double ;
-            # unit = char_string ;
-            # receiver = node_name | 'Vector__XXX' ;
-
             self.__message_transmitters = None
             self.__environment_variables = None
             self.__environment_variables_data = None
             self.__signal_types = None
 
             self.__comments = list()
-            # comments = {comment} ;
-            # comment = 'CM_' (char_string |
-            #      'BU_' node_name char_string |
-            #      'BO_' message_id char_string |
-            #      'SG_' message_id signal_name char_string |
-            #      'EV_' env_var_name char_string)
-            # ';' ;
             pt_comments = re.compile('(?P<signature>CM_)\s+(|BU_\s+(?P<bu>[\w_\d]+)|BO_\s+(?P<bo>[\w_\d]+)|SG_\s+(?P<sg>[\w_\d]+)\s+(?P<sg1>[\w_]+)|EV_\s+(?P<ev>[\w_\d]+))\s+\"(?P<char_string>.+?)\";', re.M|re.S)
             for m in pt_comments.finditer(self.contents):
                 comment = {'signature':m.group('signature'),
